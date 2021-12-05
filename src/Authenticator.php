45acdf0a87d09da2a12872222019013d0dbb9613
Kiryl Poyu <kyril.poyu@taotesting.com>
2021-12-05 15:08:35 +0100

    fix: add refresh token function, update dep

--- src/Authenticator.php
+++ src/Authenticator.php
@@ -4,37 +4,50 @@ declare(strict_types=1);
 
 namespace OlxAuthenticator;
 
-use Parhomenko\Olx\Exceptions\UnknownCountryException;
+use GuzzleHttp\Exception\GuzzleException;
+use Parhomenko\Olx\Api\User;
+use Parhomenko\Olx\ApiInterface;
+use Parhomenko\Olx\Exceptions\BadRequestException;
+use Parhomenko\Olx\Exceptions\RefreshTokenException;
 use Parhomenko\Olx\OlxFactory;
+use Throwable;
 
 class Authenticator
 {
     /** @var string|int */
     private $clientId;
     /** @var string */
-    private $clientSecret;
-    /** @var string */
     private $redirectUrl;
+    /** @var ApiInterface */
+    private $api;
 
     public function __construct($clientId, string $clientSecret, string $redirectUrl)
     {
-
         $this->clientId = $clientId;
-        $this->clientSecret = $clientSecret;
         $this->redirectUrl = $redirectUrl;
+        $this->api = $api = OlxFactory::create(OlxFactory::UA, [
+            'client_id' => $this->clientId,
+            'client_secret' => $clientSecret,
+        ]);
     }
 
     /**
      * @return string
-     * @throws UnknownCountryException
      */
     public function getOauthLink(): string
     {
-        $api = OlxFactory::create(OlxFactory::UA, [
-            'client_id' => $this->clientId,
-            'client_secret' => $this->clientSecret,
-        ]);
+        return $this->api->getUser()->getOAuthLink($this->redirectUrl, (string) $this->clientId);
+    }
 
-        return $api->getUser()->getOAuthLink($this->redirectUrl, (string) $this->clientId);
+    /**
+     * @return User
+     * @throws GuzzleException
+     * @throws BadRequestException
+     * @throws RefreshTokenException
+     * @throws Throwable
+     */
+    public function refreshToken(): User
+    {
+        return $this->api->getUser()->refreshToken();
     }
 }
